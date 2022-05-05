4a9fbfdc047d0097afbab804c86399eb24bdb7d9
Irish Moore <jasonamoore@frontier.com>
2022-05-04 20:44:06 -0400

    modified navbar and completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import { NavBar } from './components/Navbar/Navbar';
+import { Home } from './components/Home/Home'
+import { GetStarted } from './components/GetStarted/GetStarted'
+import { Login } from './components/Login/Login'
 
 
 
 function App() {
   return (
-    <div>
-      <NavBar />
-        
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavBar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/getstarted" element={<GetStarted />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
