2855907edd68cedad50869ee384ff5088ff9fd34
Emily <emilyadarr@gmail.com>
2022-05-07 11:28:35 -0400

    changes to server to look into graphql

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const routes = require('./controllers');
 // import ApolloServer
-//const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 // import our typeDefs and resolvers
-// const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
-// const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
 
 // cors
@@ -16,6 +16,21 @@ var cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+
 
 // Cors
 app.use(cors());
