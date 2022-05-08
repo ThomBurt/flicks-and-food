5c63a04e73626eb5408808c0f68ea7d4218ab34c
besharpe5 <besharpe15@gmail.com>
2022-05-07 23:23:22 -0400

    swiper setup

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import '../Home/index.css';
-import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
+import {Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay';
 
 export default Home; () => {
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
+      modules={[Pagination, Autoplay]}
+      slidesPerView={1}
       pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
+      autoplay={true}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+      <SwiperSlide>
+        <img className="img" src="client/public/images" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="img" src="client/public/images" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="img" src="client/public/images" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="img" src="client/public/images" alt="" />
+      </SwiperSlide>
     </Swiper>
   );
 };
