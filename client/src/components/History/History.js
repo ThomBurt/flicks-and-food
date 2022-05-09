5830dd70bf2964b516817897dfb2b2f145fb3acc
Irish Moore <jasonamoore@frontier.com>
2022-05-09 14:45:46 -0400

    updated history page with model info

--- client/src/components/History/History.js
+++ client/src/components/History/History.js
@@ -1,25 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {
     HistoryWrapper,
     HistoryContainerForm,
     HistoryList
 } from './History.elements'
 
-export const History = () => {
+export const History = ({ history }) => {
 
     return (
         <div>
             <HistoryWrapper>
                 <HistoryContainerForm>
                     <HistoryList>
-                        <p> 
-                        Test #1<br></br>
-                        Test #1<br></br>
-                        Test #1<br></br>
-                        Test #1<br></br>
-                        Test #1<br></br>
-                        Test #1<br></br> 
-                        </p>
+                        <Link to={`/history/${history._id}`}>
+                        <p>{history.historyText}</p>
+                        </Link>
                     </HistoryList>
                 </HistoryContainerForm>
             </HistoryWrapper>
