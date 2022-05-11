a237127bf1869d715058e7e28fd1de97b682b5f2
besharpe5 <besharpe15@gmail.com>
2022-05-10 20:01:34 -0400

    swiper images loading correctly

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {LogoHome} from './LogoHome/LogoHome';
+// import {LogoHome} from './LogoHome/LogoHome';
 // import {BgPicture} from './BgPicture/BgPicture';
 
-import {Pagination, Autoplay } from 'swiper';
+import { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
+import './Home.css';
+//import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 
+
+const images = [
+    { image: require("/Users/bensharpe/Desktop/flicks-and-food/client/src/components/Assets/img/movie-slide.jpeg") },
+    { image: require("/Users/bensharpe/Desktop/flicks-and-food/client/src/components/Assets/img/food-slide.jpg") },
+    { image: require("/Users/bensharpe/Desktop/flicks-and-food/client/src/components/Assets/img/drink-slide.jpg")},
+    { image: require("/Users/bensharpe/Desktop/flicks-and-food/client/src/components/Assets/img/final-slide.jpg")}
+  ];
 
 export const Home = () => {
 
@@ -18,28 +26,29 @@ export const Home = () => {
             {/*<LogoHome /> */}
             <Swiper
       // install Swiper modules
-      modules={[Pagination, Autoplay]}
+      modules={[Autoplay]}
       slidesPerView={1}
+      //spaceBetween={50}
       pagination={{ clickable: true }}
       autoplay={true}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>
-        <img className="img" src="client/public/images/movie-slide.jpeg" alt="" />
+      <SwiperSlide className="swiper-container">
+        <img className="img" src={images[0].image} alt="" />
         <h3>Choose the perfect movie</h3>
       </SwiperSlide>
-      <SwiperSlide>
+      <SwiperSlide className="swiper-container">
         <h3>Pair it with some food</h3>
-        <img className="img" src="client/public/images/food-slide.jpg" alt="" />
+        <img className="img" src={images[1].image} alt="" />
       </SwiperSlide>
-      <SwiperSlide>
-        <img className="img" src="client/public/images/drink-slide.jpg" alt="" />
+      <SwiperSlide className="swiper-container">
+        <img className="img" src={images[2].image} alt="" />
         <h3>Top it off with a drink</h3>
       </SwiperSlide>
-      <SwiperSlide>
-        <h2>Get Started!</h2>
-        <img className="img" src="client/public/images/final-slide.jpg" alt="" />
+      <SwiperSlide className="swiper-container">
+        <h3>Enjoy!</h3>
+        <img className="img" src={images[3].image} alt="" />
       </SwiperSlide>
     </Swiper>
         </div>
