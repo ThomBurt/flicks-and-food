9005cdea483f286473460d778105e0ae3a51d8e4
Irish Moore <jasonamoore@frontier.com>
2022-05-11 23:29:11 -0400

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
